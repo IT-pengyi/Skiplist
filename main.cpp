1a3b34499a707c7711c68a1eacc9ab355aebfeb7
IT-pengyi <95164241+IT-pengyi@users.noreply.github.com>
2022-08-03 20:04:55 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,15 +11,11 @@ int main() {
 
     SkipList<int, string> skipList(10);
     skipList.insert_element(1, "学"); 
-	skipList.insert_element(13, "算法"); 
-	skipList.insert_element(7, "认准"); 
-	skipList.insert_element(18, "微信公众号：代码随想录"); 
-	skipList.insert_element(9, "学习"); 
-	skipList.insert_element(49, "算法不迷路"); 
-	skipList.insert_element(19, "赶快关注吧你会发现详见很晚！"); 
+    skipList.insert_element(13, "算法"); 
+    
 
     skipList.insert_element(17, "学习"); 
-	skipList.insert_element(18, "Redis");
+    skipList.insert_element(18, "Redis");
     skipList.insert_element(19, "跳跃表");
     skipList.insert_element(99, "OK");
     skipList.insert_element(919, "XXX");
